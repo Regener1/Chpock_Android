6d2e850d653c07aac6c4e0c8699885f0864544e8
Regener1 <st.8.sh@mail.ru>
2017-06-17 19:15:14 +0300

    actionbar style

--- app/src/main/java/com/example/user/chpok/ScreenSlidePageFragment.java
+++ app/src/main/java/com/example/user/chpok/ScreenSlidePageFragment.java
@@ -19,8 +19,6 @@ public class ScreenSlidePageFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-
-
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_screen_slide_page, container, false);
 
